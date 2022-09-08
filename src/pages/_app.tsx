afc72e85db1d3ddea324f9e9c57e72da9f6798ce
Jefferson <jeferson.oliveira@g.globo>
2022-09-08 00:24:34 -0300

    criando header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
+import { Container, Header } from "../styles/pages/app";
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Container>
+      <Header>
+        <span>Jeje - Shop</span>
+      </Header>
       <Component {...pageProps} />
-    </>
+    </Container>
   );
 }
