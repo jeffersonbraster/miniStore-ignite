2df8d782186ccfeda2c737e8d520b98c2ea5b06f
Jefferson <jeferson.oliveira@g.globo>
2022-09-09 03:36:37 -0300

    onfigurando slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,22 @@
 import Image from "next/future/image";
+import { useKeenSlider } from "keen-slider/react";
 import { HomeContainer, Product } from "../styles/pages/home";
+import "keen-slider/keen-slider.min.css";
 import camiseta1 from "../assets/1.png";
 import camiseta2 from "../assets/2.png";
-import camiseta3 from "../assets/1.png";
+import camiseta3 from "../assets/3.png";
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 2,
+      spacing: 48,
+    },
+  });
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image
           src={camiseta1}
           width={520}
@@ -21,9 +30,23 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image
           src={camiseta2}
+          width={520}
+          height={480}
+          alt="camiseta rocket 2"
+        />
+
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 100,00</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image
+          src={camiseta3}
           width={520}
           height={480}
           alt="camiseta rocket 2"
