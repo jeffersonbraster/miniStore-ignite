f885cfe86b8657ff98c9c40b6ab47973647723c5
Jefferson <jeferson.oliveira@g.globo>
2022-09-10 01:05:52 -0300

    estilizando a pagina de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,11 +1,30 @@
 import { useRouter } from "next/router";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../../styles/pages/product";
 
 export default function Product() {
   const { query } = useRouter();
 
   return (
-    <div>
-      <h1>{JSON.stringify(query)}</h1>
-    </div>
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 200,00</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. At nihil,
+          blanditiis veniam sunt eum maxime numquam. Atque earum corrupti
+          molestias esse ad eius sequi necessitatibus rem eos, autem labore
+          dicta.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
   );
 }
